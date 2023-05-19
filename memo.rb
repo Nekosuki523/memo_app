17e0dcfb906a56be0edaea7bb5dc8930237c5e65
Nekosuki523 <130686806+Nekosuki523@users.noreply.github.com>
2023-05-19 18:46:53 +0900

    memo.rb

---
+++ memo.rb
@@ -0,0 +1,40 @@
+require "csv" # CSVファイルを扱うためのライブラリを読み込んでいます
+
+puts "1 → 新規でメモを作成する / 2 → 既存のメモを編集する"
+
+memo_type = gets.to_i
+
+# if文を使用して続きを作成していきましょう。
+# 「memo_type」の値（1 or 2）によって処理を分岐させていきましょう。
+
+if memo_type == 1
+    
+    puts "拡張子を除いたファイル名を入力してください"
+    file_name = gets.chomp
+    
+    puts "メモの内容を記入してください。Ctrl+Dで保存します。"
+    memo_type = gets.chomp
+    memo = readlines
+    
+    CSV.open("#{file_name}.csv", "w") do |csv|
+        csv << [memo]
+        
+    end
+    
+elsif memo_type == 2
+    
+    puts "既存のメモを編集します。拡張子を除いた既存ファイル名を入力してください"
+    file_name = gets.chomp
+    
+    puts "編集したい内容を入力してください。入力後、Ctrl＋Cを押してください"
+    memo_type = gets.chomp
+    memo = readlines
+    
+    CSV.open("#{file_name}.csv", "w") do |csv|
+        csv << [memo]
+    end
+    
+else
+    puts "1か2の数字を入力してください"
+    
+end
